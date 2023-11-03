4b70ec8bb6c58ff56555bcfabbee31cd3d27329c
arjunchandrank <arjunchandran.ck@gmail.com>
2023-11-03 13:29:53 +0530

    Initial commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 python manage.py migrate
 python manage.py collectstatic --no-input
 # python manage.py runserver 0.0.0.0:8000
-gunicorn django_azure.wsgi:application --bind 0.0.0.0:8000
+gunicorn django_azure.wsgi:application --bind 0.0.0.0:8080
